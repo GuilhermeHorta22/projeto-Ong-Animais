c0de85e41a6c6ff7cc6f04ce93eb0db065b8bfe9
GuilhermeHorta22 <ghorta800@gmail.com>
2025-10-23 16:53:05 -0300

    FEAT(AdotantePage): Adiciona sidebar tipo adotante

--- FrontEnd/projetoOngAnimais/src/pages/Usuarios/AdotantePage.jsx
+++ FrontEnd/projetoOngAnimais/src/pages/Usuarios/AdotantePage.jsx
@@ -1,11 +1,21 @@
+import { Outlet } from "react-router-dom";
 import AnimaisPage from "../Animais/AnimaisPage";
+import Sidebar from "../../components/Sidebar";
+import { useAuthGuard } from "../../validation/useAuthGuard";
 
 function AdotantePage()
 {
+    const isAuthorized = useAuthGuard("ADOTANTE");
+
+    if(isAuthorized === false)
+        return null;
 
     return (
-        <div>
-            <AnimaisPage/>
+        <div className="flex">
+            <Sidebar />
+                <div className="flex-1 bg-slate-300 min-h-screen p-6">
+                    <Outlet />
+                </div>
         </div>
     );
 }
