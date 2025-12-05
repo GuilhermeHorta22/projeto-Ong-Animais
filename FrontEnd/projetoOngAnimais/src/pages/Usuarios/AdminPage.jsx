3d3ca5284da00e8e59a03a2ccb61942b93be443c
GuilhermeHorta22 <ghorta800@gmail.com>
2025-12-04 21:10:54 -0300

    refactor: tirando importações desnecessarias.

--- FrontEnd/projetoOngAnimais/src/pages/Usuarios/AdminPage.jsx
+++ FrontEnd/projetoOngAnimais/src/pages/Usuarios/AdminPage.jsx
@@ -1,26 +1,25 @@
-import { Outlet, useNavigate } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import Sidebar from "../../components/Sidebar";
 import { Edit3, Info } from 'lucide-react';
 import AnimaisPage from "../Animais/AnimaisPage";
 import { useAuthGuard } from "../../utils/validation/useAuthGuard";
-import { useState, useEffect } from "react";
 
 function AdminPage()
 {
     const isAuthorized = useAuthGuard("ADMIN");
 
-    //se a pessoa nao tiver autorizacao mantem na pagina atual
+    //volta para a pagina porque não tem autorização
     if(isAuthorized === false)
         return null;
 
     return (
         <div className="flex">
             <Sidebar />
-            <div className="flex-1 bg-slate-300 min-h-screen p-6">
-                <Outlet />
+                <div className="flex-1 bg-slate-300 min-h-screen p-6">
+                    <Outlet />
+                </div>
             </div>
-        </div>
-    );
+        );
 }
 
 export default AdminPage;
