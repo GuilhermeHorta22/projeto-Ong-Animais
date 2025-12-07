5d2f00295e60a881802e27e4d4c6f387939c7448
GuilhermeHorta22 <ghorta800@gmail.com>
2025-12-07 18:38:57 -0300

    style: mudança de cor na borda.

--- FrontEnd/projetoOngAnimais/src/components/Select.jsx
+++ FrontEnd/projetoOngAnimais/src/components/Select.jsx
@@ -1,7 +1,7 @@
 function Select({ className="", ...props})
 {
     return (
-        <select {...props} className={`${className || "bg-stone-100 text-stone-900 border border-white-800 outline-slate-400 px-4 py-2 rounded-md w-full"}`}>
+        <select {...props} className={`${className || "bg-stone-100 text-stone-900 border border-slate-400 outline-slate-400 px-4 py-2 rounded-md w-full"}`}>
             {props.children}
         </select>
     );
