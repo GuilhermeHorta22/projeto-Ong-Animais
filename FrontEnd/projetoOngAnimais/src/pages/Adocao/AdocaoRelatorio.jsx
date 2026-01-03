32d56822fce7ce2466506edb1658adaec53ba7c9
GuilhermeHorta22 <ghorta800@gmail.com>
2026-01-03 00:41:45 -0300

    style: mudança na escrita dos campos da tabela de adoção.

--- FrontEnd/projetoOngAnimais/src/pages/Adocao/AdocaoRelatorio.jsx
+++ FrontEnd/projetoOngAnimais/src/pages/Adocao/AdocaoRelatorio.jsx
@@ -109,10 +109,10 @@ function AdocaoRelatorio()
                 <table className="min-w-full divide-y divide-gray-900">
                     <thead className="bg-gray-50">
                         <tr>
-                            <th className="px-6 py-3 text-left text-xs font-medium text-gray-700 uppercase tracking-wider">Código adoção</th>
-                            <th className="px-6 py-3 text-left text-xs font-medium text-gray-700 uppercase tracking-wider">Nome Animal</th>
-                            <th className="px-6 py-3 text-left text-xs font-medium text-gray-700 uppercase tracking-wider">Nome Usuário</th>
-                            <th className="px-6 py-3 text-left text-xs font-medium text-gray-700 uppercase tracking-wider">Data adoção</th>
+                            <th className="px-6 py-3 text-left text-xs font-medium text-gray-700 uppercase tracking-wider">Código da adoção</th>
+                            <th className="px-6 py-3 text-left text-xs font-medium text-gray-700 uppercase tracking-wider">Nome do Animal</th>
+                            <th className="px-6 py-3 text-left text-xs font-medium text-gray-700 uppercase tracking-wider">Nome do Adotante</th>
+                            <th className="px-6 py-3 text-left text-xs font-medium text-gray-700 uppercase tracking-wider">Data da adoção</th>
                             <th className="px-6 py-3 text-center text-xs font-medium text-gray-700 uppercase tracking-wider">Ações</th>
                         </tr>
                     </thead>
