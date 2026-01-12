c8b8619f1b19103f143d276891b66c36db7d6678
GuilhermeHorta22 <ghorta800@gmail.com>
2026-01-12 14:53:53 -0300

    feat(AuthRoutes): adiciona novas routes para forgotPassword e resetPassword.

--- BackEnd/src/routes/AuthRoutes.ts
+++ BackEnd/src/routes/AuthRoutes.ts
@@ -1,8 +1,14 @@
 import { Router } from 'express';
-import { login } from '../controllers/AuthController.js';
+import { 
+    login,
+    forgotPassword,
+    resetPassword
+ } from '../controllers/AuthController.js';
 
 const router = Router();
 
 router.post('/login', login);
+router.post('/forgot-password', forgotPassword);
+router.post('/reset-password/:token', resetPassword);
 
 export default router;
