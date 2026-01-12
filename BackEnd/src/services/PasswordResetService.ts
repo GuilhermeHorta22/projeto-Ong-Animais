91732c624a950e582caea05204b79f151d72c461
GuilhermeHorta22 <ghorta800@gmail.com>
2026-01-12 14:55:04 -0300

    fix(PasswordResetService): corrige falta de virgula no método de criar novo password_reset.

--- BackEnd/src/services/PasswordResetService.ts
+++ BackEnd/src/services/PasswordResetService.ts
@@ -4,7 +4,7 @@ import {CreatePasswordResetDTO} from '../dtos/CreatePasswordResetDTO.js';
 export class PasswordResetService {
     async criar(data: CreatePasswordResetDTO): Promise<void> {
         await pool.query(
-            `INSERT INTO password_reset (usuario_id, token expires_at)
+            `INSERT INTO password_reset (usuario_id, token, expires_at)
             VALUES ($1, $2, $3)`, [data.usuario_id, data.token, data.expires_at]
         );
     }
