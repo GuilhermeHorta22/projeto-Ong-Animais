08d4246e1ea6013d12993a4ecb275d69a6256e9d
GuilhermeHorta22 <ghorta800@gmail.com>
2026-01-13 12:54:15 -0300

    feat(Email): Adiciona tratamento de exceções para o envio do email.

--- BackEnd/src/utils/Email.ts
+++ BackEnd/src/utils/Email.ts
@@ -1,18 +1,31 @@
 import nodemailer from 'nodemailer';
 
 export const sendEmail = async (to: string, subject: string, text: string) => {
-    const transporter = nodemailer.createTransport({
-        service: "gmail",
-        auth: {
-            user: process.env.EMAIL_USER,
-            pass: process.env.EMAIL_PASS
-        }
-    });
+    try
+    {
+        const transporter = nodemailer.createTransport({
+            service: "gmail",
+            auth: {
+                user: process.env.EMAIL_USER,
+                pass: process.env.EMAIL_PASS
+            },
+            tls: {
+                rejectUnauthorized: false
+            }
+        });
 
-    await transporter.sendMail({
-        from: `"Sistema de Adoção" <${process.env.EMAIL_USER}>`,
-        to,
-        subject,
-        text
-    });
+        await transporter.sendMail({
+            from: `"Sistema de Adoção" <${process.env.EMAIL_USER}>`,
+            to,
+            subject,
+            text
+        });
+
+        console.log("Email enviado com sucesso!");
+    }
+    catch(error)
+    {
+        console.error("Erro ao enviar email: ", error);
+        throw error;
+    }
 }
